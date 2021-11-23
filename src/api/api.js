2a52606a40d2d74d864109749396f31917af7032
faoberilo <89050385+faoberilo@users.noreply.github.com>
2021-11-22 21:58:47 -0300

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-    apiUrl: 'http://localhost:3001/tarefas',
+    apiUrl: 'https://bluetodolistbackend.herokuapp.com/tarefas',
     fetchGetAll: () => fetch(Api.apiUrl),
     fetchGetById: (id) => fetch(`${Api.apiUrl}/${id}`),
     fetchPost: (tarefa) => {
